e669921e7a0d3a6cdaed29ac667218b89895c8f4
Filip Tepper <filip@tepper.pl>
2021-01-21 09:28:26 +0100

    add provider bloc,

--- main.tf
+++ main.tf
@@ -7,3 +7,12 @@ resource "aws_instance" "instance2" {
   name          = "test"
   instance_type = "t2.small"
 }
+
+terraform {
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
