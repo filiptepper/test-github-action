04d1d7100a6c0810d6444b5198c4fa32a68afc7e
Filip Tepper <filip@tepper.pl>
2021-01-21 09:24:59 +0100

    add more for testing

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
 resource "aws_instance" "myTest-Instance" {
   name          = "test"
- instance_type = "t2.small"
+  instance_type = "t2.small"
+}
+
+resource "aws_instance" "myTest-Instance2" {
+  name          = "test"
+  instance_type = "t2.small"
 }
